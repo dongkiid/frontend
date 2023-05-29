80bebebff4ce1dd05ac90f4519d9ea33abd4a1a2
Baekys97 <ys0907kr@naver.com>
2023-05-29 11:36:24 +0900

    (feat:  로그인 된 상태일때 axios헤더에 accesstoken 추가)

--- src/lib/api.tsx
+++ src/lib/api.tsx
@@ -1,4 +1,5 @@
 import axios from 'axios'
+import Cookies from 'js-cookie';
 
 const api = axios.create({
 	baseURL : 'http://localhost:5000/',
@@ -6,5 +7,17 @@ const api = axios.create({
         "Content-Type": `application/json`,
       },
 })
+
+//로그인 된 상태일때 header에 인증토큰 추가하는 인터셉터
+api.interceptors.request.use((config) => {
+  const key = Cookies.get("key");
+
+  if (key) {
+    config.headers.Authorization = `Bearer ${key}`;
+  }
+
+  return config;
+});
+
 
 export default api
