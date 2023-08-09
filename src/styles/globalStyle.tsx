22e39e7d7a5a7480e5db20de82ba5ff38ff240bf
Baekys97 <ys0907kr@naver.com>
2023-08-09 19:37:44 +0900

    (feat: 로고 및 폰트 src 추가)

--- src/styles/globalStyle.tsx
+++ src/styles/globalStyle.tsx
@@ -1,28 +1,28 @@
 import { createTheme } from "@mui/material/styles";
 
 const theme = createTheme({
-    palette: {
-        primary: {
-            main: "#FFAE8B"
-        },
-        secondary: {
-            main: "#767676"
-        },
-        error: {
-            main: "#DA1E28"
-        },
-        background: {
-            default: "#ffffff"
-          }
+  palette: {
+    primary: {
+      main: "#FFAE8B"
     },
-    typography: {
-        fontFamily: [ "NanumBarunGothic","SDSamliphopangche_Basic","SDSamliphopangche_Outline","Noto Sans KR", "sans-serif", "-apple-system", "BlinkMacSystemFont", "Segoe UI", "Roboto", "Oxygen", "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue"].join(",")
-        ,
-        allVariants: {
-            color: '#404040'
-          }
+    secondary: {
+      main: "#767676"
     },
-   components: {
+    error: {
+      main: "#DA1E28"
+    },
+    background: {
+      default: "#ffffff"
+    }
+  },
+  typography: {
+    fontFamily: ["NanumBarunGothic", "SDSamliphopangche_Basic", "SDSamliphopangche_Outline", "Noto Sans KR", "sans-serif", "-apple-system", "BlinkMacSystemFont", "Segoe UI", "Roboto", "Oxygen", "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue"].join(",")
+    ,
+    allVariants: {
+      color: '#404040'
+    }
+  },
+  components: {
     MuiCssBaseline: {
       styleOverrides: `
         @font-face {
@@ -36,11 +36,25 @@ const theme = createTheme({
             font-weight: normal;
             font-style: normal;
         };
-        
+        @font-face {
+          font-family: 'NanumBarunGothic';
+          font-style: normal;
+          font-weight: 400;
+          src: url('//cdn.jsdelivr.net/font-nanumlight/1.0/NanumBarunGothicWeb.eot');
+          src: url('//cdn.jsdelivr.net/font-nanumlight/1.0/NanumBarunGothicWeb.eot?#iefix') format('embedded-opentype'), url('//cdn.jsdelivr.net/font-nanumlight/1.0/NanumBarunGothicWeb.woff') format('woff'), url('//cdn.jsdelivr.net/font-nanumlight/1.0/NanumBarunGothicWeb.ttf') format('truetype');
+         };
+         
+         @font-face {
+          font-family: 'NanumBarunGothic';
+          font-style: normal;
+          font-weight: 700;
+          src: url('//cdn.jsdelivr.net/font-nanumlight/1.0/NanumBarunGothicWebBold.eot');
+          src: url('//cdn.jsdelivr.net/font-nanumlight/1.0/NanumBarunGothicWebBold.eot?#iefix') format('embedded-opentype'), url('//cdn.jsdelivr.net/font-nanumlight/1.0/NanumBarunGothicWebBold.woff') format('woff'), url('//cdn.jsdelivr.net/font-nanumlight/1.0/NanumBarunGothicWebBold.ttf') format('truetype')
+         };
       `,
     },
-    
-}
+
+  }
 });
 
 
