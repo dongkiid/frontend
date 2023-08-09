23bab5636a054d44c01c71bead89612aab14a6c4
Baekys97 <ys0907kr@naver.com>
2023-08-09 19:40:38 +0900

    (layout: 메인페이지에 컴포넌트 추가)

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,13 +1,26 @@
+import { Box, Container } from '@mui/material';
+import BoardList from 'components/main/BoardList';
+import Calender from 'components/main/Calender';
 import * as React from 'react';
+import { styled } from 'styled-components';
 
 const MainPage: React.FC = () => {
   return (
-    <div>
-      <main>
-        <h2>메인 페이지</h2>
-      </main>
-    </div>
+      <MainBox>
+        <Calender />
+        <BoardList />
+      </MainBox>
   );
 };
 
 export default MainPage;
+
+
+const MainBox = styled(Container)({
+  display: 'flex',
+  flexDirection: 'column',
+  justifyContent: 'center',
+  alignItems: 'center',
+  textAlign: 'center',
+  marginBottom: '20rem'
+});
